bfb314bbf0c10d3e34f17f642397b82d5c9ef365
vivoulia <viviendebuchy@yahoo.fr>
2019-06-05 22:09:48 +0200

    Environnement de dev

---
+++ Makefile
@@ -0,0 +1,12 @@
+# TP1 : Fichier Makefile
+#
+include ../Makefile.inc
+
+EXE = serveur client
+
+all: ${EXE}
+
+clean:
+	rm -f *.o *~ ${EXE}
+
+
---
+++ serveur.c
@@ -0,0 +1,11 @@
+#include "pse.h"
+
+int main()
+{
+<<<<<<< HEAD
+	printf("Hello World\n");
+=======
+	printf("Hello World 2 \n");
+>>>>>>> master
+	return 0;
+}
