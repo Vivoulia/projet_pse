eb8b841a6aa933b0a5316dc53c2c633b32d55a17
vivoulia <viviendebuchy@yahoo.fr>
2019-06-10 16:04:23 +0200

    Client lecture/ecriture
    Client basique qui ecoute le ecrit au serveur

--- client.c
+++ client.c
@@ -10,6 +10,7 @@ typedef struct Thread {
 	sem_t sem;
 	sem_t sem_w;
 	sem_t sem_r;
+	int fin;
 } Thread_Data;
 
 
@@ -17,10 +18,16 @@ void *listenServer(void *arg)
 {
 	Thread_Data *tc = (Thread_Data*) arg;
 	char buf[BUF_SIZE];
-	int nbRead;
-	int continu =1;
-	
-	while (continu);
+	int nbRead = 0;
+	while (1)
+	{
+		nbRead = lireLigne(tc->canal,buf);
+		if(nbRead == -1)
+			erreur_IO("lireLigne");
+		printf("Reception: %s", buf);
+		//On a reçu un message on laisse le client répondre
+		sem_post(&tc->sem_w);
+	}
 	
 	exit(EXIT_SUCCESS);
 }
@@ -29,29 +36,38 @@ void *sendServer(void *arg)
 {
 	Thread_Data *tc = (Thread_Data*) arg;
 	char buf[BUF_SIZE];
-	int nbRead;
-	int continu = 1;
+	int nbWrite = 0;
+	//Discussion entre le client et le serveur
 	
-	while (continu);
+	while(strcmp(buf, "fin\n") != 0)
+	{
+		sem_wait(&tc->sem_w);
+		printf(">");
+		if(fgets(buf, LIGNE_MAX, stdin) == NULL)
+			erreur("fegts null\n");
+		nbWrite = ecrireLigne(tc->canal, buf);
+		if(nbWrite == -1)
+			erreur_IO("ecrireLigne");
+		printf("%s: Commande envoyé au serveur\n", CMD);
+	}
+	
+
 	
 	exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[])
 {	
-	int soc, ret;
+	int canal, ret;
 	struct sockaddr_in* adrServ;
-	char ligne[BUF_SIZE];
-	int lgEcr;
-	int nbWrite = 0;
-	int nbRead = 0;
+	Thread_Data thread_data;
 	
 	if(argc != 3)
 		erreur("usage: %s machine port\n", argv[0]);
 	
 	printf("%s: Création du socket\n", CMD);
-	soc = socket(AF_INET, SOCK_STREAM, 0);
-	if(soc < 0)
+	canal = socket(AF_INET, SOCK_STREAM, 0);
+	if(canal < 0)
 		erreur_IO("socket");
 	
 	printf("%s: Résolution DNS pour %s sur le port %s\n", CMD, argv[1], argv[2]);
@@ -62,28 +78,27 @@ int main(int argc, char *argv[])
 	printf("%s : Adresse %s, port %hu\n", CMD, stringIP(ntohl(adrServ->sin_addr.s_addr)), ntohs(adrServ->sin_port));
 	
 	printf("%s: Connection au socket\n", CMD);
-	ret = connect(soc, (struct sockaddr *) adrServ, sizeof(struct sockaddr_in));
+	ret = connect(canal, (struct sockaddr *) adrServ, sizeof(struct sockaddr_in));
 	if(ret<0)
 		erreur_IO("connect");
 		
-	//Discussion entre le client et le serveur
-	/*
-	while(strcmp(ligne, "fin\n") != 0)
-	{
-		printf(">");
-		if(fgets(ligne, LIGNE_MAX, stdin) == NULL)
-			erreur("fegts null\n");
-		lgEcr = ecrireLigne(soc, ligne);
-		if(lgEcr == -1)
-			erreur_IO("ecrireLigne");
-		printf("Message envoyé au serveur\n");
-	}*/
-	nbRead = lireLigne(soc,ligne);
-	if(nbRead == -1)
-		erreur_IO("lireLigne");
-	printf("Reception: %s", ligne);
+	thread_data.tid = 1;
+	thread_data.canal = canal;
+	sem_init(&thread_data.sem, 0, 0);
+	sem_init(&thread_data.sem_w, 0, 0);
+	sem_init(&thread_data.sem_r, 0, 0);
+	if (pthread_create(&thread_data.Rid, NULL, listenServer, &thread_data) != 0) 
+		erreur_IO("creation of the reading thread");
+	printf("Lancement du thread de lecture n°%d | ",thread_data.tid);
+	if (pthread_create(&thread_data.Wid, NULL, sendServer, &thread_data) != 0)
+		erreur_IO("creation of the writing thread");
+	printf("Lancement du thread d'écriture n°%d\n", thread_data.tid);
+	
+	//attente de la fin des thread
+	pthread_join(thread_data.Wid, NULL);
 		
-	if (close(soc) == -1)
+	if (close(canal) == -1)
 		erreur_IO("cloe");
+		
 	return 0;
 }
